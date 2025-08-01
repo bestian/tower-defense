be51d9a4e65878325c0b9773025e3f00d8b3cbcd
Bestian Tang <109048+bestian@users.noreply.github.com>
2025-08-01 09:59:32 +0800

    deploy

--- assets/AboutView-BLJs2igk.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as o,a as t,o as a}from"./index-bPDve5KI.js";const n={},r={class:"about"};function c(i,e){return a(),o("div",r,e[0]||(e[0]=[t("h1",null,"This is an about page",-1),t("a",{href:"https://github.com/bestian/tower-defense"},"GitHub",-1)]))}const _=s(n,[["render",c]]);export{_ as default};
